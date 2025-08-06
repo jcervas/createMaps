05815c3426756daaa89328aabd1d5503d33c9f2e
Jonathan Cervas <jcervas@andrew.cmu.edu>
2025-08-06 16:28:55 -0400

    Update gerrymander_map.r

--- gerrymanders/gerrymander_map.r
+++ gerrymanders/gerrymander_map.r
@@ -31,7 +31,7 @@ fips <- data.frame(
     state <- c("FL", "GA", "IL", "KS", "LA", "NC", "NM", "NV", "OH", "OR", "SC", "TN", "TX", "UT", "WI")
     color <- "#EA655D"
 
-fips$color <- ifelse(fips$abv %in% state, color, "none")  # Default color for other states
+fips$color <- ifelse(fips$abv %in% state, color, "#fff")  # Default color for other states
 
 write.csv(fips, '/Users/cervas/Library/Mobile Documents/com~apple~CloudDocs/Downloads/gerrymanders.csv', row.names=FALSE)
 
