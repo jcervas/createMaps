a53c51020f5fa64186af6597c0f2f61c3b71012f
Jonathan Cervas <jcervas@andrew.cmu.edu>
2025-08-06 16:27:50 -0400

    Update gerrymander_map.r

--- gerrymanders/gerrymander_map.r
+++ gerrymanders/gerrymander_map.r
@@ -1,63 +1,31 @@
+# ------------------------------------------------------------------------------
+# gerrymander_map.r
+#
+# This script creates a data frame containing US states and territories with their
+# corresponding FIPS codes and abbreviations. It then marks a subset of states
+# (D/F states) with a specific color code and exports the resulting data frame
+# as a CSV file.
+#
+# Main Steps:
+# 1. Define a data frame `fips` with columns: state name, abbreviation, and FIPS code.
+# 2. Specify a vector of state abbreviations (`state`) representing D/F states.
+# 3. Assign a color ("#EA655D") to D/F states and "none" to all others in a new column.
+# 4. Export the annotated data frame to a CSV file.
+#
+# Output:
+# - CSV file containing state names, abbreviations, FIPS codes, and color annotation.
+#
+# Author: [Your Name]
+# Date: [Date]
+# ------------------------------------------------------------------------------
 # Read fips data
 
-state,abv,fips
-Alabama,AL,01
-Alaska,AK,02
-Arizona,AZ,04
-Arkansas,AR,05
-California,CA,06
-Colorado,CO,08
-Connecticut,CT,09
-Delaware,DE,10
-Florida,FL,12
-Georgia,GA,13
-Hawaii,HI,15
-Idaho,ID,16
-Illinois,IL,17
-Indiana,IN,18
-Iowa,IA,19
-Kansas,KS,20
-Kentucky,KY,21
-Louisiana,LA,22
-Maine,ME,23
-Maryland,MD,24
-Massachusetts,MA,25
-Michigan,MI,26
-Minnesota,MN,27
-Mississippi,MS,28
-Missouri,MO,29
-Montana,MT,30
-Nebraska,NE,31
-Nevada,NV,32
-New Hampshire,NH,33
-New Jersey,NJ,34
-New Mexico,NM,35
-New York,NY,36
-North Carolina,NC,37
-North Dakota,ND,38
-Ohio,OH,39
-Oklahoma,OK,40
-Oregon,OR,41
-Pennsylvania,PA,42
-Rhode Island,RI,44
-South Carolina,SC,45
-South Dakota,SD,46
-Tennessee,TN,47
-Texas,TX,48
-Utah,UT,49
-Vermont,VT,50
-Virginia,VA,51
-Washington,WA,53
-West Virginia,WV,54
-Wisconsin,WI,55
-Wyoming,WY,56
-American Samoa,AS,60
-Guam,GU,66
-Northern Mariana Islands,MP,69
-Puerto Rico,PR,72
-Virgin Islands,VI,78
-
-fips <- read.csv('/Users/cervas/Library/CloudStorage/GoogleDrive-jcervas@andrew.cmu.edu/My Drive/GitHub/Data/fips.csv', colClasses=c(fips="character"))
+fips <- data.frame(
+    state = c("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming", "American Samoa", "Guam", "Northern Mariana Islands", "Puerto Rico", "Virgin Islands"),
+    abv = c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "AS", "GU", "MP", "PR", "VI"),
+    fips = c("01", "02", "04", "05", "06", "08", "09", "10", "12", "13", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "44", "45", "46", "47", "48", "49", "50", "51", "53", "54", "55", "56", "60", "66", "69", "72", "78"),
+    stringsAsFactors = FALSE
+)
 
 # Data for D/F states
     state <- c("FL", "GA", "IL", "KS", "LA", "NC", "NM", "NV", "OH", "OR", "SC", "TN", "TX", "UT", "WI")
